7f68fdaa0e9c2cebc87a6f151e5e42e43b66a93b
James White <whitejaad@gmail.com>
2022-07-10 07:36:55 -0700

    Save variable

--- variable.rb
+++ variable.rb
@@ -3,3 +3,6 @@ security_type = "Classified"
 
 Wolverine_year_of_birth = 1932
 Wolverine_weight = 300
+
+Wolverine_says = "I am the best at what I do"
+Wolverine_groups = "X-men, Avengers, short stint with Fantastic Four"
